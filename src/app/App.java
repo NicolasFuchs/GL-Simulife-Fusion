01633a45e3a7bcdce3f75e877216acb2898023f9
DESKTOP-I1TJ68L\gmich <guillaume.michel@edu.hefr.ch>
2017-11-29 17:11:02 +0100

    add joption

--- src/app/App.java
+++ src/app/App.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import ch.eiafr.gl.simulife.gui.UserInterface;
 import ch.eiafr.gl.simulife.gui.game.GameGraphicView;
 import ch.eiafr.gl.simulife.gui.game.GameMatrixView;
@@ -15,40 +18,45 @@ import world.MyWorld;
 
 public class App {
 
-  private static final int         NumberOfIce = 10;
-  private static Orca              orca;
-  private static WhiteShark[]      whiteSharks;
-  private static HammerheadShark[] hammerheadSharks;
-  private static Penguin[]         penguins;
-  private static ArrayList<Ice>    iceList;
-  private static boolean           game_not_finished;
-  private static int               loop_id;
-  private static Random            random;
-  private static MyWorld           myWorld;
+	private static final int NumberOfIce = 10;
+	private static Orca orca;
+	private static WhiteShark[] whiteSharks;
+	private static HammerheadShark[] hammerheadSharks;
+	private static Penguin[] penguins;
+	private static ArrayList<Ice> iceList;
+	private static boolean game_not_finished;
+	private static int loop_id;
+	private static Random random;
+	private static MyWorld myWorld;
 
-  public static void main(String[] args) {
-    boolean isChessLife = false;
+	public static void main(String[] args) {
+		boolean isChessLife;
+		JFrame frame = new JFrame();
+		String[] options = new String[2];
+		options[0] = new String("Chess");
+		options[1] = new String("Waterworld");
+		if (JOptionPane.showOptionDialog(frame.getContentPane(), "Choose a game","Simulif", 0,JOptionPane.QUESTION_MESSAGE,
+				null,options,null) == JOptionPane.YES_OPTION) {
+			isChessLife = true;
+		} else {
+			isChessLife = false;
+		}
 
-    if (args.length > 0) {
-      String arg = args[0];
-      if (arg.equalsIgnoreCase("true") || arg.equalsIgnoreCase("false"))
-        isChessLife = Boolean.valueOf(arg);
-    }
-    
-    myWorld = new MyWorld(8, 8, isChessLife);
 
-    UserInterface userGraphiqueInterface = new UserInterface(
-        new GameGraphicView(myWorld.getNbCols(), myWorld.getNbRows()));
-    myWorld.addObserver(userGraphiqueInterface);
+		myWorld = new MyWorld(8, 8, isChessLife);
 
-    UserInterface userGameMatrixView = new UserInterface(
-        new GameMatrixView(myWorld.getNbCols(), myWorld.getNbRows()));
-    myWorld.addObserver(userGameMatrixView);
+		UserInterface userGraphiqueInterface = new UserInterface(
+				new GameGraphicView(myWorld.getNbCols(), myWorld.getNbRows()));
+		myWorld.addObserver(userGraphiqueInterface);
 
-    UserInterface userTextInterface = new UserInterface(new GameTextView());
-    myWorld.addObserver(userTextInterface);
+		UserInterface userGameMatrixView = new UserInterface(
+				new GameMatrixView(myWorld.getNbCols(), myWorld.getNbRows()));
+		myWorld.addObserver(userGameMatrixView);
 
-    myWorld.init(isChessLife);
-    myWorld.updateView();
-  }
+		UserInterface userTextInterface = new UserInterface(new GameTextView());
+		myWorld.addObserver(userTextInterface);
+
+		myWorld.init(isChessLife);
+		myWorld.updateView();
+	}
 }
