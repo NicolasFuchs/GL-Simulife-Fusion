d97e113a3ce082ffbd49fa9ba8caa34556eb708f
Andrea Ruffini <andreamarco.ruffini@edu.hefr.ch>
2017-12-04 09:40:05 +0100

    Added some GUI modifications

---
+++ src/GUI/MyGameGraphicView.java
@@ -0,0 +1,75 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import ch.eiafr.gl.simulife.gui.game.IGameView;
+import ch.eiafr.gl.simulife.model.AWorld;
+import ch.eiafr.gl.simulife.model.ICreature;
+
+public class MyGameGraphicView implements IGameView {
+
+  private JFrame     m_Frame = new JFrame("Simulife - GameGraphicView");
+  private JPanel     m_ButtonPanel;
+  private JPanel[][] m_grid;
+
+  public MyGameGraphicView(int nbCols, int nbRows, boolean isChessLife) {
+    this.m_Frame.setDefaultCloseOperation(3);
+    this.m_ButtonPanel = new JPanel(new GridLayout(nbRows, nbCols));
+    this.m_grid = new JPanel[nbCols][nbRows];
+
+    for (int y = 0; y < nbRows; ++y) {
+      for (int x = 0; x < nbCols; ++x) {
+        this.m_grid[x][y] = new JPanel(new GridLayout(1, 1));
+        if (isChessLife) {
+          if ((x + y) % 2 == 0) {
+            this.m_grid[x][y].setBackground(Color.LIGHT_GRAY);
+          } else {
+            this.m_grid[x][y].setBackground(Color.GRAY);
+          }
+        } else {
+          this.m_grid[x][y].setBackground(Color.CYAN);
+        }
+        this.m_ButtonPanel.add(this.m_grid[x][y]);
+      }
+    }
+
+    this.m_Frame.add(this.m_ButtonPanel);
+    this.m_Frame.setLocation(50, 200);
+    // this.m_Frame.setSize(nbCols * 75, nbRows * 95 + 20);
+    this.m_Frame.setSize(nbCols * 95, nbRows * 95);
+    this.m_Frame.setVisible(true);
+  }
+
+  public void drawNextStep(AWorld world) {
+    int col;
+    int row;
+    for (col = 0; col < world.getNbCols(); ++col) {
+      for (row = 0; row < world.getNbRows(); ++row) {
+        this.m_grid[col][row].removeAll();
+        this.m_grid[col][row].repaint();
+      }
+    }
+
+    for (col = 0; col < world.getNbCols(); ++col) {
+      for (row = 0; row < world.getNbRows(); ++row) {
+        this.m_grid[col][row].removeAll();
+        ICreature creature = world.getCreatureAt(col, row);
+        if (creature != null) {
+          ImageIcon temp = world.getIconeFor(creature);
+          this.m_grid[col][row].add(new JLabel(temp));
+          this.m_grid[col][row].validate();
+          this.m_grid[col][row].repaint();
+        }
+      }
+    }
+
+    this.m_Frame.repaint();
+  }
+
+}
--- src/app/App.java
+++ src/app/App.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import GUI.MyGameGraphicView;
 import ch.eiafr.gl.simulife.gui.UserInterface;
 import ch.eiafr.gl.simulife.gui.game.GameGraphicView;
 import ch.eiafr.gl.simulife.gui.game.GameMatrixView;
@@ -18,45 +19,45 @@ import world.MyWorld;
 
 public class App {
 
-	private static final int NumberOfIce = 10;
-	private static Orca orca;
-	private static WhiteShark[] whiteSharks;
-	private static HammerheadShark[] hammerheadSharks;
-	private static Penguin[] penguins;
-	private static ArrayList<Ice> iceList;
-	private static boolean game_not_finished;
-	private static int loop_id;
-	private static Random random;
-	private static MyWorld myWorld;
+  private static final int         NumberOfIce = 10;
+  private static Orca              orca;
+  private static WhiteShark[]      whiteSharks;
+  private static HammerheadShark[] hammerheadSharks;
+  private static Penguin[]         penguins;
+  private static ArrayList<Ice>    iceList;
+  private static boolean           game_not_finished;
+  private static int               loop_id;
+  private static Random            random;
+  private static MyWorld           myWorld;
 
-	public static void main(String[] args) {
-		boolean isChessLife;
-		JFrame frame = new JFrame();
-		String[] options = new String[2];
-		options[0] = new String("Chess");
-		options[1] = new String("Waterworld");
-		if (JOptionPane.showOptionDialog(frame.getContentPane(), "Choose a game","Simulif", 0,JOptionPane.QUESTION_MESSAGE,
-				null,options,null) == JOptionPane.YES_OPTION) {
-			isChessLife = true;
-		} else {
-			isChessLife = false;
-		}
+  public static void main(String[] args) {
+    boolean isChessLife;
+    JFrame frame = new JFrame();
+    String[] options = new String[2];
+    options[0] = new String("Chess");
+    options[1] = new String("Waterworld");
+    if (JOptionPane.showOptionDialog(frame.getContentPane(), "Choose a game",
+        "Simulif", 0, JOptionPane.QUESTION_MESSAGE, null, options,
+        null) == JOptionPane.YES_OPTION) {
+      isChessLife = true;
+    } else {
+      isChessLife = false;
+    }
 
+    myWorld = new MyWorld(8, 8, isChessLife);
 
-		myWorld = new MyWorld(8, 8, isChessLife);
+    UserInterface userGameMatrixView = new UserInterface(
+        new GameMatrixView(myWorld.getNbCols(), myWorld.getNbRows()));
+    myWorld.addObserver(userGameMatrixView);
+    
+    UserInterface userGraphiqueInterface = new UserInterface(
+          new MyGameGraphicView(myWorld.getNbCols(), myWorld.getNbRows(), isChessLife));
+    myWorld.addObserver(userGraphiqueInterface);
 
-		UserInterface userGraphiqueInterface = new UserInterface(
-				new GameGraphicView(myWorld.getNbCols(), myWorld.getNbRows()));
-		myWorld.addObserver(userGraphiqueInterface);
+    UserInterface userTextInterface = new UserInterface(new GameTextView());
+    myWorld.addObserver(userTextInterface);
 
-		UserInterface userGameMatrixView = new UserInterface(
-				new GameMatrixView(myWorld.getNbCols(), myWorld.getNbRows()));
-		myWorld.addObserver(userGameMatrixView);
-
-		UserInterface userTextInterface = new UserInterface(new GameTextView());
-		myWorld.addObserver(userTextInterface);
-
-		myWorld.init(isChessLife);
-		myWorld.updateView();
-	}
+    myWorld.init(isChessLife);
+    myWorld.updateView();
+  }
 }
