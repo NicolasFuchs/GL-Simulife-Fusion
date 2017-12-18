b1f973f5fb4ca10ea28857e90eaa0e954f194b8e
Andrea Ruffini <andreamarco.ruffini@edu.hefr.ch>
2017-12-18 11:41:20 +0100

    Added grid size change

--- src/app/App.java
+++ src/app/App.java
@@ -6,12 +6,14 @@ import ch.eiafr.gl.simulife.gui.game.GameMatrixView;
 import ch.eiafr.gl.simulife.gui.game.GameTextView;
 import world.MyWorld;
 
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class App {
 
-  private static MyWorld           myWorld;
-  
+  private static MyWorld myWorld;
+
   public static void main(String[] args) {
     boolean isChessLife;
     JFrame frame = new JFrame();
@@ -29,37 +31,71 @@ public class App {
     options[0] = new String("Step by step");
     options[1] = new String("Auto");
     if (JOptionPane.showOptionDialog(frame.getContentPane(), "Choose a mode",
-            "Simulif", 0, JOptionPane.QUESTION_MESSAGE, null, options,
-            null) == JOptionPane.YES_OPTION) {
+        "Simulife", 0, JOptionPane.QUESTION_MESSAGE, null, options,
+        null) == JOptionPane.YES_OPTION) {
       stepByStep = true;
     } else {
       stepByStep = false;
     }
-    //TODO ADD here a input for col and row
-
-    myWorld = new MyWorld(8, 8, isChessLife, stepByStep);
+    // TODO ADD here a input for col and row
+    int[] size = new int[2];
+    size[0] = 8;
+    size[1] = 8;
+    if (!isChessLife)
+      size = getGridSize();
+    myWorld = new MyWorld(size[0], size[1], isChessLife, stepByStep);
 
     options[0] = new String("Matrix View");
     options[1] = new String("Graphique View");
     if (JOptionPane.showOptionDialog(frame.getContentPane(), "Choose a view",
-            "Simulif", 0, JOptionPane.QUESTION_MESSAGE, null, options,
-            null) == JOptionPane.YES_OPTION) {
+        "Simulif", 0, JOptionPane.QUESTION_MESSAGE, null, options,
+        null) == JOptionPane.YES_OPTION) {
       UserInterface userGameMatrixView = new UserInterface(
-              new GameMatrixView(myWorld.getNbCols(), myWorld.getNbRows()));
+          new GameMatrixView(myWorld.getNbCols(), myWorld.getNbRows()));
       myWorld.addObserver(userGameMatrixView);
     } else {
       UserInterface userGraphiqueInterface = new UserInterface(
-              new MyGameGraphicView(myWorld.getNbCols(), myWorld.getNbRows(), isChessLife));
+          new MyGameGraphicView(myWorld.getNbCols(), myWorld.getNbRows(),
+              isChessLife));
       myWorld.addObserver(userGraphiqueInterface);
     }
-
-
-
 
     UserInterface userTextInterface = new UserInterface(new GameTextView());
     myWorld.addObserver(userTextInterface);
 
     myWorld.init(isChessLife);
     myWorld.updateView();
+  }
+
+  private static int[] getGridSize() {
+    Integer[] sizes = new Integer[13];
+    for (int i = 0; i < sizes.length; i++) {
+      sizes[i] = i + 8;
+    }
+    
+    int[] size = new int[2];
+    size[0] = sizes[0];
+    size[1] = sizes[0];
+    
+    JComboBox<Integer> cbX = new JComboBox<>(sizes);
+    JComboBox<Integer> cbY = new JComboBox<>(sizes);
+
+    JPanel myPanel = new JPanel();
+    myPanel.add(new JLabel("x:"));
+    myPanel.add(cbX);
+    myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+    myPanel.add(new JLabel("y:"));
+    myPanel.add(cbY);    
+    
+    int res = JOptionPane.showConfirmDialog(null, myPanel, "Choose grid size",
+        JOptionPane.DEFAULT_OPTION);
+
+
+    // If a string was returned, say so.
+    if (res == JOptionPane.DEFAULT_OPTION) {
+      size[0] = (int) cbX.getSelectedItem();
+      size[1] = (int) cbY.getSelectedItem();
+    }
+    return size;
   }
 }
