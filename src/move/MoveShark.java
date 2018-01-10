4a151b80bd8cd329c7fa7c7b61fc3f9e6d6568ef
guillaum.michel <Guillaume.Michel@edu.hefr.ch>
2018-01-10 23:10:09 +0100

    change shark

--- src/move/MoveShark.java
+++ src/move/MoveShark.java
@@ -44,7 +44,12 @@ public class MoveShark extends Move {
                 }
                 if (foundPenguin) break;
             }
-            if (foundPenguin) break;
+            if (foundPenguin) {
+                list.remove(game[newY][newX]);
+                if (white != null) white.eat();
+                else if (hammer != null) hammer.eat();
+                break;
+            }
             if (isValidPoint(c.getPosition().x-2)) {
                 foundPenguin = true;
                 newX = c.getPosition().x-2;
@@ -62,12 +67,7 @@ public class MoveShark extends Move {
                 newX = c.getPosition().x;
                 newY = c.getPosition().y+2;
             }
-            if (foundPenguin) {
-                list.remove(game[newY][newX]);
-                if (white != null) white.eat();
-                else if (hammer != null) hammer.eat();
-                break;
-            }
+      
 
             
             dx = rd.nextInt(3) - 1;
